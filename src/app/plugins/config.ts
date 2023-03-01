ec638550ce3efe478778d17d7396e24b4f78b163
hibohiboo <swdice66@gmail.com>
2023-03-01 19:04:16 +0900

    add PL Mode

--- src/app/plugins/config.ts
+++ src/app/plugins/config.ts
@@ -1,4 +1,5 @@
 const params = new URL(document.URL).searchParams;
 export const pluginConfig = {
   is2d: params.get('2d') != null,
+  forPL: params.get('pl') != null,
 } as const;
--- src/app/plugins/extends/component/master-screen/master-screen.component.ts
+++ src/app/plugins/extends/component/master-screen/master-screen.component.ts
@@ -1,24 +1,25 @@
 
 import { Component } from '@angular/core';
+import { pluginConfig } from 'src/app/plugins/config';
 
 @Component({
   selector: 'app-wrapper',
-  template: ` <div style="display: flex;position: relative;;">
-                <div style="width:600px;height: 100vh;">
+  template: ` <div style="display: flex;position: relative;">
+                <div *ngIf="!isPLMode" style="width:600px;height: 100vh;">
                   <board-list></board-list>
                 </div>
                 <div style="width: 100%;overflow: hidden; position: relative;">
-                  <div style="width: 100%; height:80vh;border: 2px solid #222;overflow: hidden; position: relative;">
+                  <div [style.height]="isPLMode ? '100vh' : '80vh'" style="width: 100%; ;border: 2px solid #222;overflow: hidden; position: relative;">
                    <app-root></app-root>
                   </div>
-                  <div>
+                  <div *ngIf="!isPLMode">
                     <scene-list></scene-list>
                   </div>
               </div>`,
 })
 
 export class MasterScreenComponent {
-
+  get isPLMode() { return pluginConfig.forPL; }
   constructor() {
   }
 
